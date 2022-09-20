87af910e77778bedf6c0d35c9537faa65c2db382
lindennerd <lindennerd@gmail.com>
2022-09-20 14:35:02 -0300

    feat: add cors to apollo server

--- apps/server/src/apollo-server.js
+++ apps/server/src/apollo-server.js
@@ -2,5 +2,11 @@ import apollo from "apollo-server-express";
 import resolvers from "./resolvers/index.js";
 import typeDefs from "./typeDefs/index.js";
 
-const server = new apollo.ApolloServer({ typeDefs, resolvers });
+const server = new apollo.ApolloServer({
+  typeDefs,
+  resolvers,
+  cors: {
+    origin: ["*"],
+  },
+});
 export default server;
