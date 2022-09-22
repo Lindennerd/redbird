995eb802f1674e1c4737ca0060f18826b017155f
lindennerd <lindennerd@gmail.com>
2022-09-22 07:34:09 -0300

    feat: setting up tailwind

---
+++ web/config/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#EF3109',
+        secondary: '#C32301',
+      },
+    },
+  },
+  plugins: [],
+}
