52dfb89841561f28971c41533033ef1650b68a5c
Luiz Paulo Lindenmaier da Silva <luizpsilva1@kpmg.com.br>
2022-09-26 08:54:02 -0300

    fixing click on reply button

--- web/src/components/Tweet/Tweet.tsx
+++ web/src/components/Tweet/Tweet.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Link, routes } from '@redwoodjs/router'
 import LikeButton from '../LikeButton/LikeButton'
 import ReplyButton from '../ReplyButton/ReplyButton'
 import ReplyTweet from '../ReplyTweet/ReplyTweet'
@@ -29,26 +30,28 @@ interface TweetProps {
 }
 
 const Tweet = (props: TweetProps) => {
-  const [toggleReplyModal, setToggleReplyModal] = useState(false);
+  const [toggleReplyModal, setToggleReplyModal] = useState(false)
 
   function dateFormat(datetime: string) {
-  const parsedDate = new Date(datetime)
-  const month = parsedDate.toLocaleString('default', { month: 'long' })
-  return `${parsedDate.getDate()} ${month} ${parsedDate.getFullYear()}`
+    const parsedDate = new Date(datetime)
+    const month = parsedDate.toLocaleString('default', { month: 'long' })
+    return `${parsedDate.getDate()} ${month} ${parsedDate.getFullYear()}`
   }
 
   return (
     <div className="w-full rounded-md border">
-      <div className="flex justify-between p-2">
-        <span className="text-sm text-gray-500">{props.tweet.user.name}</span>
-        <span className="text-sm text-gray-500">
-          {dateFormat(props.tweet.createdAt)}
-        </span>
+      <div className="flex justify-between p-2 hover:cursor-pointer hover:bg-slate-100">
+        <Link to={routes.tweet({ id: props.tweet.id })}>
+          <span className="text-sm text-gray-500">{props.tweet.user.name}</span>
+          <span className="text-sm text-gray-500">
+            {dateFormat(props.tweet.createdAt)}
+          </span>
+          <div className="px-4 py-2">{props.tweet.text}</div>
+        </Link>
       </div>
-      <div className="px-4 py-2">{props.tweet.text}</div>
       {props.displayActions && (
         <div className="flex items-center justify-between p-2">
-          <div className='flex items-center'>
+          <div className="flex items-center">
             <ReplyButton onClick={() => setToggleReplyModal(true)} />
             <span>{props.tweet._count.replies}</span>
           </div>
