169887b875c88652442654c5f69f6551c32f12c1
Luiz Paulo Lindenmaier da Silva <luizpsilva1@kpmg.com.br>
2022-09-27 12:00:29 -0300

    implementing retweet ui

--- web/src/components/RetweetButton/RetweetButton.tsx
+++ web/src/components/RetweetButton/RetweetButton.tsx
@@ -1,13 +1,37 @@
-import { FaRetweet } from "react-icons/fa";
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import { FaRetweet } from 'react-icons/fa'
 
 const RetweetButton = () => {
   return (
-    <button
-      className="rounded-full  p-2 transition-all
+    <div>
+      <Menu as="div" className="relative inline-block text-left">
+        <Menu.Button
+          className="rounded-full  p-2 transition-all
      hover:bg-slate-200 hover:shadow-md"
-    >
-      <FaRetweet className="hover:text-primary" />
-    </button>
+        >
+          <FaRetweet className="hover:text-primary" />
+        </Menu.Button>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Menu.Item>
+              <button>Retweet</button>
+            </Menu.Item>
+            <Menu.Item>
+              <button>Retweet with a comment</button>
+            </Menu.Item>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
   )
 }
 
