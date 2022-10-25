9ba350c991dfcf81618bff88e215587f93cba48a
Luiz Paulo Lindenmaier da Silva <luizpsilva1@kpmg.com.br>
2022-10-25 08:58:16 -0300

    feat:tweet size limit frontend

--- web/src/components/TweetForm/TweetForm.tsx
+++ web/src/components/TweetForm/TweetForm.tsx
@@ -1,4 +1,5 @@
 import { useAuth } from '@redwoodjs/auth'
+import { toast } from '@redwoodjs/web/dist/toast'
 import { useState } from 'react'
 
 interface TweetInput {
@@ -11,29 +12,45 @@ interface TweetFormProps {
   onLoading: boolean
 }
 
+const tweetLimitSize = 160
+
 const TweetForm = ({ onPost, onLoading }: TweetFormProps) => {
   const { currentUser } = useAuth()
   const [tweet, setTweet] = useState('')
 
   function onSendTweet() {
+    if (tweet.length >= tweetLimitSize) {
+      toast.error('Your tweet is too long!')
+      return
+    }
+    if (tweet.length <= 0) {
+      toast.error('Your tweet is too short')
+      return
+    }
+
     onPost({
       text: tweet,
       userId: currentUser.id,
-    });
+    })
+    setTweet('')
+  }
 
-    setTweet('');
+  function handleChange(e) {
+    setTweet((tweet) => e.target.value)
   }
 
   return (
-    <div className="rounded-md border p-2 shadow-md dark:border-gray-900 dark:text-white bg-white dark:bg-gray-800">
+    <div className="rounded-md border bg-white p-2 shadow-md dark:border-gray-900 dark:bg-gray-800 dark:text-white">
       <textarea
-        className="w-full rounded-md p-2 outline-none border focus:shadow-xl dark:bg-gray-700 dark:border-gray-900"
+        className="w-full rounded-md border p-2 outline-none focus:shadow-xl dark:border-gray-900 dark:bg-gray-700"
         placeholder="What's going on?"
         name="tweet"
         value={tweet}
-        onChange={(e) => setTweet(e.target.value)}
+        onChange={handleChange}
       />
-      <div></div>
+      <div className={tweet.length >= tweetLimitSize ? `text-red-600`: `dark:text-gray-400 text-gray-700`}>{`${
+        tweetLimitSize - tweet.length
+      } characters left`}</div>
       <div className="flex justify-between">
         <div>Tweet Stuff</div>
         <button
